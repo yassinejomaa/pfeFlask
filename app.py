f134f0af39d0120b2457b115c004704094fb137e
Fedi <Fedi.abidi2@gmail.com>
2025-05-01 19:47:14 +0100

    add predict expense model

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from transformers import pipeline
 from flask_cors import CORS
 from expense_advisor import ExpenseAdvisor 
 import torch
+from sklearn.preprocessing import MinMaxScaler
+import pickle
+import numpy as np
 
 # Initialize Flask app
 device = 0 if torch.cuda.is_available() else -1  # Utiliser 0 pour le GPU, -1 pour CPU
@@ -48,6 +51,35 @@ def generate_advice():
 
     return Response(advisor.generate_advice_stream(expenses), mimetype='text/plain')
 
+model = pickle.load(open("model_file.pkl", "rb"))
+
+@app.route('/predict_expense/<int:input1>/<int:input2>/<int:input3>/<int:input4>', methods=['GET'])
+def predict_expense(input1, input2, input3, input4):
+
+    # Step 1: Prepare your input and min/max values
+    input_values = [[input1, input2, input3, input4]]
+    min_vals = [82.72, 0, 1, 0]
+    max_vals = [86650.2,2, 26, 8]
+
+    # Step 2: Fit MinMaxScaler using min/max as fake dataset
+    scaler = MinMaxScaler()
+    scaler.fit([min_vals, max_vals])  # Fit on boundary values only
+
+    # Step 3: Transform the input
+    normalized_input = scaler.transform(input_values)[0]
+
+    # Convert the input to a NumPy array
+    input_array = np.array([normalized_input])
+
+    # Make the prediction
+    prediction = model.predict(input_array)
+
+    # Print and Return the response
+    print("the predicted Expenses is:", prediction)
+    response = {
+        'prediction': prediction.tolist()
+    }
+    return jsonify(response)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
